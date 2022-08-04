d8bd7afacbbc5d54e8c84b6dbcd2755a7c4d8788
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-04 13:24:55 +0530

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+   int var;
+   printf("Enter var\t");
+   scanf("%d",&var);
+   switch(var)
+   {
+       case 1:
+        printf("good");
+        break;
+       case 2:
+        printf("better");
+        break;
+       case 3:
+        printf("best");
+        break;
+       default:
+        printf("Invalid");
+        break;
+   }
+    return 0;
+}
