dcff12d9a54fbdafaa84193e89dfe6c2c191dba8
devanshshekhar00 <75908373+devanshshekhar00@users.noreply.github.com>
2022-08-04 12:20:50 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main()
+{
+   int monthNumber;printf("Enter month number\t");
+   scanf("%d",&monthNumber);
+   switch(monthNumber)
+   {
+       case 1 ... 7:
+        if(monthNumber%2==0)
+        {
+            printf("30 days");
+            break;
+        }
+        else 
+        {
+            printf("31 days");
+            break;
+        }
+       case 8 ... 12:
+        if(monthNumber%2!=0)
+        {
+            printf("30 days");
+            break;
+        }
+        else 
+        {
+            printf("31 days");
+            break;
+        }
+        
+   }
+
+    return 0;
+}
